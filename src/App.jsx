23b7a3452354a0e0d0aef81746246e2f77b2019e
emoncse2020 <emon.cse.2020@gmail.com>
2025-05-18 00:38:44 +0600

    exploring

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import DaisyNavBar from "./components/DaisyNavBar/DaisyNavBar";
+
+function App() {
+  return (
+    <>
+      <DaisyNavBar></DaisyNavBar>
+    </>
+  );
+}
+
+export default App;
